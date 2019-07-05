0d55c9efb7668ba0d00b365fd245c9997d3b00ec
tranconghoaxx <51305900+tranconghoaxx@users.noreply.github.com>
2019-07-05 10:32:24 +0700

    Add project files.

---
+++ webgitttt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webgitttt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
